1b59ece8a10ac9de24920b5c246784b8d30b7cdc
Ludwig <ludwig.pusl@gmail.com>
2018-04-15 01:05:06 +0200

    fix

--- imageprocessing/viewcamera.py
+++ imageprocessing/viewcamera.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
 vs = cv2.VideoCapture(0)
-#cv2.VideoCapture.set(CV_CAP_PROP_FRAME_WIDTH, 4416)
-#cv2.VideoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 1242)
-print(cv2.VideoCapture.get(CV_CAP_PROP_FRAME_WIDTH))
-cv2.VideoCapture.set(CV_CAP_PROP_FRAME_WIDTH, 4416)
-cv2.VideoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 1242)
+if not(vs.isOpened()):
+    vs.open()
+
+if hasattr(cv2, 'cv'):
+    vs.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 4416)
+    vs.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1242)
+else:
+    vs.set(cv2.CAP_PROP_FRAME_WIDTH, 4416)
+    vs.set(cv2.CV_CAP_PROP_FRAME_HEIGHT, 1242)
 while True:
     ok, frame = vs.read()
     frame = frame[0:376, 0:650]
@@ -27,18 +31,19 @@ while True:
     im2, contours, hierarchy = cv2.findContours(maskc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
-    for c in contours:
-        # compute the center of the contour
-        M = cv2.moments(c)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+    if not contours:
+        for c in contours:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
 
-        # draw the contour and center of the shape on the image
-        # cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-        # cv2.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
-        # cv2.putText(frame, "center", (cX - 20, cY - 20),
-        #			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        cv2.drawMarker(frame, (cX, cY), (0, 255, 0), markerSize=200, thickness=20)
+            # draw the contour and center of the shape on the image
+            # cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+            # cv2.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
+            # cv2.putText(frame, "center", (cX - 20, cY - 20),
+            #			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.drawMarker(frame, (cX, cY), (0, 255, 0), markerSize=200, thickness=20)
 
     cv2.imshow("frame", frame)
     cv2.waitKey(2)
