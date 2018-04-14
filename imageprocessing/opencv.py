505433698aa078707b3038ef5efc521f97d50ada
Chris <34374252+christqoh@users.noreply.github.com>
2018-04-14 21:27:35 +0200

    more images

--- imageprocessing/opencv.py
+++ imageprocessing/opencv.py
@@ -4,18 +4,23 @@ import numpy as np
 
 
 while(1):
-	frame = cv2.imread('photo7.JPG')
+	frame = cv2.imread('photo12.jpg')
 	#img2 = img.astype(np.float32)
 	#print(img2.dtype)
 
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	lower = np.array([-30,100,150])
-	upper = np.array([30,255,255])
+	lower = np.array([0,100,100])
+	upper = np.array([5,255,255])
+	lower2 = np.array([170,100,100])
+	upper2 = np.array([180,255,255])
 
 	mask = cv2.inRange(hsv, lower, upper)
+	mask2 = cv2.inRange(hsv, lower2, upper2)
 
-	res = cv2.bitwise_and(frame,frame, mask= mask)
+	maskc = cv2.add(mask,mask2)
+
+	res = cv2.bitwise_and(frame,frame, mask= maskc)
 
 	cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
 	cv2.resizeWindow('frame', 1000,1000)
