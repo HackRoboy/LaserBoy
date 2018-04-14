87d633c0eed0ad5d75296621d4b75e48f2d127b8
Ludwig <ludwig.pusl@gmail.com>
2018-04-14 21:59:12 +0200

    semifunctional

--- imageprocessing/opencv.py
+++ imageprocessing/opencv.py
@@ -21,9 +21,22 @@ maskc = cv2.add(mask,mask2)
 
 res = cv2.bitwise_and(frame,frame, mask= maskc)
 
-#im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+im2, contours, hierarchy = cv2.findContours(maskc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+for c in contours:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+
+	# draw the contour and center of the shape on the image
+	#cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+	#cv2.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
+	#cv2.putText(frame, "center", (cX - 20, cY - 20),
+	#			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+	cv2.drawMarker(frame,(cX, cY),(0, 255, 0),markerSize=200,thickness=20)
+	print("hallo:" + cX + " " + cY)
 #print(contours.__len__())
 #cv2.drawContours(im2, contours, 3, (0,255,0), 3)
 #cv2.drawContours(im2, contours, -1,(0,0,255),3)
@@ -31,11 +44,11 @@ res = cv2.bitwise_and(frame,frame, mask= maskc)
 cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('frame', 600,600)
 
-cv2.namedWindow('res',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('res', 600,600)
+cv2.namedWindow('maskc',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('maskc', 600,600)
 
 cv2.imshow('frame',frame)
-cv2.imshow('res',res)
+cv2.imshow('maskc',maskc)
 #cv2.imshow('res',res)
 while(1):
 	k = cv2.waitKey(5) & 0xFF
