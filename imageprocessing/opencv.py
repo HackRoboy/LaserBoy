3949d3a2beebe43fd471921e73ec4bcb8ada048f
Ludwig <ludwig.pusl@gmail.com>
2018-04-14 21:31:43 +0200

    buh

--- imageprocessing/opencv.py
+++ imageprocessing/opencv.py
@@ -3,35 +3,41 @@ import numpy as np
 
 
 
+frame = cv2.imread('photo12.jpg')
+#img2 = img.astype(np.float32)
+#print(img2.dtype)
+
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+lower = np.array([0,100,100])
+upper = np.array([5,255,255])
+lower2 = np.array([170,100,100])
+upper2 = np.array([180,255,255])
+
+mask = cv2.inRange(hsv, lower, upper)
+mask2 = cv2.inRange(hsv, lower2, upper2)
+
+maskc = cv2.add(mask,mask2)
+
+res = cv2.bitwise_and(frame,frame, mask= maskc)
+
+#im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+#print(contours.__len__())
+#cv2.drawContours(im2, contours, 3, (0,255,0), 3)
+#cv2.drawContours(im2, contours, -1,(0,0,255),3)
+
+cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('frame', 600,600)
+
+cv2.namedWindow('res',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('res', 600,600)
+
+cv2.imshow('frame',frame)
+cv2.imshow('res',res)
+#cv2.imshow('res',res)
 while(1):
-	frame = cv2.imread('photo12.jpg')
-	#img2 = img.astype(np.float32)
-	#print(img2.dtype)
-
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-	lower = np.array([0,100,100])
-	upper = np.array([5,255,255])
-	lower2 = np.array([170,100,100])
-	upper2 = np.array([180,255,255])
-
-	mask = cv2.inRange(hsv, lower, upper)
-	mask2 = cv2.inRange(hsv, lower2, upper2)
-
-	maskc = cv2.add(mask,mask2)
-
-	res = cv2.bitwise_and(frame,frame, mask= maskc)
-
-	cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
-	cv2.resizeWindow('frame', 1000,1000)
-
-	cv2.namedWindow('res',cv2.WINDOW_NORMAL)
-	cv2.resizeWindow('res', 1000,1000)
-
-	cv2.imshow('frame',frame)
-	#cv2.imshow('mask',mask)
-	cv2.imshow('res',res)
-
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
 		break
