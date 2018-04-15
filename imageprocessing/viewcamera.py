e6bdc7ca7c0899c45c4f16e531d00b05cef73205
andreas.janiak <andreas_janiak@hotmail.com>
2018-04-15 10:51:53 +0200

    fix

--- imageprocessing/viewcamera.py
+++ imageprocessing/viewcamera.py
@@ -12,13 +12,13 @@ else:
     vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 1242)
 while True:
     ok, frame = vs.read()
-    frame = frame[0:376, 0:650]
+    frame = frame[0:1242, 0:2208]
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower = np.array([0, 100, 100])
+    lower = np.array([0, 200, 200])
     upper = np.array([5, 255, 255])
-    lower2 = np.array([170, 100, 100])
+    lower2 = np.array([170, 200, 200])
     upper2 = np.array([180, 255, 255])
 
     mask = cv2.inRange(hsv, lower, upper)
@@ -31,10 +31,10 @@ while True:
     im2, contours, hierarchy = cv2.findContours(maskc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
-    if not contours:
-        for c in contours:
-            # compute the center of the contour
-            M = cv2.moments(c)
+    if contours:
+        # compute the center of the contour
+        M = cv2.moments(contours[0])
+        if M["m00"] != 0:
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
 
@@ -44,7 +44,10 @@ while True:
             # cv2.putText(frame, "center", (cX - 20, cY - 20),
             #			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             cv2.drawMarker(frame, (cX, cY), (0, 255, 0), markerSize=200, thickness=20)
-
+    else:
+        print("Nothing found")
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('frame', 600,600)
     cv2.imshow("frame", frame)
     cv2.waitKey(2)
 cv2.destroyAllWindows()
